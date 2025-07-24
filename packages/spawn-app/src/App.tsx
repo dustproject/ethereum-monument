e843116c28d54e69051837be8ca193407b984ac9
alvarius <alvarius@lattice.xyz>
2025-07-24 13:31:11 -0600

    add spawn functionality

--- packages/spawn-app/src/App.tsx
+++ packages/spawn-app/src/App.tsx
@@ -1,7 +1,21 @@
+import { useState } from "react";
+import { spawn } from "./spawn";
+
 export default function App() {
+  const [error, setError] = useState<string | undefined>(undefined);
+  const [processing, setProcessing] = useState(true);
+
+  const handleSpawn = async () => {
+    setError(undefined);
+    setProcessing(true);
+    const { error } = await spawn();
+    setError(error);
+    setProcessing(false);
+  };
+
   return (
-    <div className="flex flex-col h-screen">
-      <p className="p-10">
+    <div className="flex flex-col h-screen justify-between">
+      <p className="pt-10 px-10">
         <img src="/10-years-ethereum.png" alt="Ethereum 10th Anniversary" />
         <br />
         In honor of Ethereum's 10 year anniversary, we are building an immortal
@@ -17,7 +31,14 @@ export default function App() {
         Spawn using the button below, and open the "Ethereum Monument" app to
         begin placing blocks according to the blueprint.
       </p>
-      <button className="bg-blue-500 text-white p-2">Spawn</button>
+      {error && <p className="text-red-500 text-center">{error}</p>}
+      <button
+        className="bg-blue-500 text-white p-2 hover:bg-blue-600 active:bg-blue-700"
+        onClick={handleSpawn}
+        disabled={processing}
+      >
+        {processing ? "Spawning..." : "Spawn at the Ethereum Monument"}
+      </button>
     </div>
   );
 }
