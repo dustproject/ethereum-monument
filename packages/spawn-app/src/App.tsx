19fd119e11a04d9756da7ef258bacbd320196c07
alvarius <alvarius@lattice.xyz>
2025-07-24 13:05:05 -0600

    add content and styling

--- packages/spawn-app/src/App.tsx
+++ packages/spawn-app/src/App.tsx
@@ -1,9 +1,23 @@
-import { appContext } from "./provider";
-
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello {appContext.userAddress}
-    </h1>
+    <div className="flex flex-col h-screen">
+      <p className="p-10">
+        <img src="/10-years-ethereum.png" alt="Ethereum 10th Anniversary" />
+        <br />
+        In honor of Ethereum's 10 year anniversary, we are building an immortal
+        monument.
+        <br />
+        <br />
+        Every contribution matters and is permanently recorded onchain. Whether
+        you're placing blocks to complete the blueprint or donating batteries to
+        power the protective force field that will preserve the monument for
+        generations.
+        <br />
+        <br />
+        Spawn using the button below, and open the "Ethereum Monument" app to
+        begin placing blocks according to the blueprint.
+      </p>
+      <button className="bg-blue-500 text-white p-2">Spawn</button>
+    </div>
   );
 }
