afea2efb62eb7b012aafbfea3e2516d66aadeef8
karooolis <hello@karolisram.com>
2025-07-25 15:58:37 +0300

    remove component library, show blueprint

--- packages/spawn-app/src/blueprint/Blueprint.tsx
+++ packages/spawn-app/src/blueprint/Blueprint.tsx
@@ -1,22 +1,8 @@
 import {
   type ObjectDefinition,
   categories,
-  encodePlayer,
   objects,
-  packVec3,
 } from "@dust/world/internal";
-import IWorldAbi from "@dust/world/out/IWorld.sol/IWorld.abi";
-import { resourceToHex } from "@latticexyz/common";
-import { isNotNull } from "@latticexyz/common/utils";
-import {
-  camelToSpaces,
-  decodeTransactionError,
-  getObjectImageUrl,
-  objectTypes,
-  truncateStr,
-} from "@permutation-city/common";
-import { TableWithColumns } from "@permutation-city/ui";
-import { useMutation } from "@tanstack/react-query";
 import { useCallback, useEffect, useMemo, useState } from "react";
 import { useDustClient } from "./useDustClient";
 import { useCursorPositionQuery } from "./useCursorPositionQuery";
@@ -65,69 +51,6 @@ export function Blueprint() {
         z: cursor?.z ?? prevCursor?.z ?? 0,
       },
     });
-  const { data: forceField, refetch: refetchForceField } = useForceFieldQuery(
-    playerPosition?.x ?? 0,
-    playerPosition?.y ?? 0,
-    playerPosition?.z ?? 0
-  );
-
-  const expandForceField = useMutation({
-    mutationFn: async () => {
-      if (!dustClient || !blueprintData || blueprintData.length === 0) return;
-      if (!forceField || forceField.current.forceField) {
-        return;
-      }
-      if (!forceField.adjacent) {
-        throw new Error(
-          "No adjacent force field found. Cannot expand force field."
-        );
-      }
-      console.log(
-        "found blueprint, but no force field, expanding force field",
-        forceField
-      );
-      const userAddress = dustClient.appContext.userAddress;
-      const userEntityId = encodePlayer(userAddress);
-
-      const result = await dustClient.provider.request({
-        method: "systemCall",
-        params: [
-          {
-            systemId: resourceToHex({
-              type: "system",
-              namespace: "",
-              name: "ForceFieldSystem",
-            }),
-            abi: IWorldAbi,
-            functionName: "addFragment",
-            args: [
-              userEntityId,
-              cityForceFieldId,
-              packVec3(forceField.adjacent.fragmentPos),
-              packVec3(forceField.current.fragmentPos),
-              "0x",
-            ],
-          },
-        ],
-      });
-      console.log("addFragment result", result);
-
-      const errorMessage = decodeTransactionError(IWorldAbi, result);
-      if (errorMessage) {
-        throw new Error(errorMessage);
-      }
-      refetchForceField();
-      return result;
-    },
-  });
-
-  // biome-ignore lint/correctness/useExhaustiveDependencies(expandForceField): suppress dependency expandForceField
-  // biome-ignore lint/correctness/useExhaustiveDependencies(dustClient): suppress dependency dustClient
-  // biome-ignore lint/correctness/useExhaustiveDependencies(blueprintData): suppress dependency blueprintData
-  // biome-ignore lint/correctness/useExhaustiveDependencies(forceField): suppress dependency forceField
-  useEffect(() => {
-    expandForceField.mutate();
-  }, [dustClient, blueprintData, forceField, expandForceField.mutate]);
 
   const setBlueprint = useCallback(async () => {
     if (!dustClient || !blueprintData) return;
@@ -153,57 +76,6 @@ export function Blueprint() {
       },
     });
   }, [blueprintData, dustClient, activeObjectTypeId]);
-
-  const blueprintItemsTableData = useMemo(() => {
-    if (!blueprintData) return [];
-
-    const uniqueObjectTypes = [
-      ...new Set(blueprintData.map(({ objectTypeId }) => objectTypeId)),
-    ];
-
-    const blueprintItemsTableData = uniqueObjectTypes
-      .map((objectTypeId) => {
-        if (objectTypeId === 1) {
-          return null;
-        }
-
-        return {
-          id: objectTypeId,
-          name: {
-            value: truncateStr(
-              camelToSpaces(objectTypes[objectTypeId]?.name).toUpperCase()
-            ),
-            icon: getObjectImageUrl(objectTypeId),
-          },
-        };
-      })
-      .filter(isNotNull);
-
-    return [
-      {
-        id: -1,
-        name: {
-          value: "SCAFFOLD (DIRT)",
-          icon: getObjectImageUrl(22),
-        },
-      },
-      {
-        id: 1,
-        name: {
-          value: "MINE",
-          icon: getObjectImageUrl(32768),
-        },
-      },
-      {
-        id: 0,
-        name: {
-          value: "ALL BUILDING BLOCKS",
-          icon: getObjectImageUrl(0),
-        },
-      },
-      ...blueprintItemsTableData,
-    ];
-  }, [blueprintData]);
 
   useEffect(() => {
     setBlueprint();
@@ -244,7 +116,7 @@ export function Blueprint() {
 
   if (!isBlueprintLoading && (!blueprintData || blueprintData.length === 0)) {
     return (
-      <p className="text-left text-white/70 uppercase leading-normal">
+      <p className="text-left uppercase leading-normal">
         You&apos;re too far from the Ethereum Monument. Come to{" "}
         <button
           type="button"
@@ -258,24 +130,5 @@ export function Blueprint() {
     );
   }
 
-  return (
-    <>
-      <TableWithColumns
-        columns={[
-          {
-            name: "Name",
-            key: "name",
-          },
-        ]}
-        activeRowId={activeObjectTypeId}
-        onClick={(item) => {
-          setActiveObjectTypeId(Number(item.id));
-          // setAutoSwitch(false); // TODO: add back
-        }}
-        data={blueprintItemsTableData}
-        loading={isBlueprintLoading}
-        searchable
-      />
-    </>
-  );
+  return <>Build the monument!</>;
 }
